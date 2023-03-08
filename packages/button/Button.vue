1510bb669da822c973f5a83548fa2510ab53a393
x1c <mikelee417@163.com>
2023-03-08 21:52:07 +0800

    update

---
+++ packages/button/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button :type="type">
+            <slot></slot>
+        </button>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'SiButton',
+    props: {
+        type: {
+            type: String,
+            default: "button"
+        }
+    }
+}
+</script>
---
+++ packages/button/index.js
@@ -0,0 +1,7 @@
+import SiButton from './Button'
+
+SiButton.install = function (app) {
+    app.component(SiButton.name, SiButton)
+}
+
+export default SiButton
